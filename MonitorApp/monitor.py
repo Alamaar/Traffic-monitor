561dc41eac7251f6e7adb7d1cd65a9694e51808e
kffijr8082095f <martti.alasaarela@gmail.com>
2022-03-31 15:44:41 +0300

    express init

--- MonitorApp/monitor.py
+++ MonitorApp/monitor.py
@@ -1,11 +1,6 @@
-from multiprocessing.dummy import active_children
-from sqlite3 import Timestamp
 import norfair
 import numpy as np
 import math
-
-
-
 
 
 class Monitor_object():
@@ -14,12 +9,13 @@ class Monitor_object():
         self.life = 10
         self.id = id
         self.class_name = class_name
-        self.position = position
+        self.positions = []
+        self.positions.append(position)
         self.speeds = []
         self.direction = None
         self.times = []
         self.times.append(timestamp)
-
+        self.lenght_calculated = False
 
     def __eq__(self, other):
 
@@ -27,47 +23,37 @@ class Monitor_object():
             return True
         return False
        
+                                              
+    def newPosition(self, position, timestamp):   
+        self.life = self.life + 1                 
+        self.times.append(timestamp) 
+        self.positions.append(position)                                                                                                     
 
+        if self.direction == None:
+            self.direction = self.set_direction(self.positions[-2],self.positions[-1])
+            
 
-    def newPosition(self, position, timestamp):
-        self.life = self.life + 1 
-        self.times.append(timestamp)
-        speed = self.calculateSpeed(self.position, position)
-        self.position = position
+    def set_direction(self, old_position, new_position):
+        direction = (new_position[0]- old_position[0])
 
-        ## Set direction
-        ## 100, 300 ( x, y)
-        ## -> <-
-        #self.direction = 
+        if direction < 0:
+            return "West"
 
-        
-
-        return speed
+        elif direction > 0:
+            return "East"  
 
 
     def calculateSpeed(self, lastPosition, newPosition):
-
-        correctionmultiply = 420 ## joku kaava keksiä
         distance = math.sqrt( ((lastPosition[0]-newPosition[0])**2)+((lastPosition[1]-newPosition[1])**2) )
-        #print(distance)
-        #print(self.times[-2])
-        #print(self.times[-1])
         speed = distance / ((self.times[-1] - self.times[-2]))
-        self.speeds.append(speed*correctionmultiply/newPosition[1])
-        #print("Speed")
-        #print(speed)
+        self.speeds.append(speed)
         return speed
         
-
-        
-
-
     def kill(self):
-        avgSpeed = sum(self.speeds)/ len(self.speeds)
+        distance = math.sqrt( ((self.positions[0][0]-self.positions[-1][0])**2)+((self.positions[0][1]-self.positions[-1][1])**2) )
+        avgSpeed = distance / ((self.times[-1] - self.times[0]))
 
         return avgSpeed
-
-
 
 
 
@@ -76,59 +62,100 @@ class Monitor_object():
 class Monitor():
 
 
-    ## Lista[car_west[[1,50], [2,39]], car_east[]]
-
-    ## flush --> 
-
-
-    def __init__(self, framerate = 30, px_to_m = 0 ):
+    def __init__(self, speed_object, object_avg_lenght , margins):
         self.monitored_objects = []
-        self.framerate = framerate
-        self.px_to_m = px_to_m
-         ## frame -> H /framerate / (1000 * px_to_m)/framerate * 3600
-        self.conversion = 1000 * px_to_m / 1/framerate * 3600
-
         self.finnished_objects = []
+        self.margins = margins
+        self.avg_lenght = object_avg_lenght
+        self.speed_object = speed_object
+
+        self.avg_w = []
+        self.avg_e = []
+
+        self.avg_set = False
+
+        self.finished_objects = []
 
 
-
-    
-
-    def update(self, new_objects, timestamp):
-
-        ## -> is same id in monitored -> if not -> add new -> if is -> addnewposition, monitired -> reduce life, 
-        ## 
-        for obj in new_objects:
-            if obj[0] in self.monitored_objects:
-                index = self.monitored_objects.index(obj[0])
-                #print("olemassa")
-                speed = self.monitored_objects[index].newPosition(obj[2], timestamp = timestamp)
-                ## get speed and add to list to return
-                #print("Speed m/s")
-                speed = speed / self.px_to_m
-                #print(speed )
-                #print("Speed km/h")
-                #print(speed * 3.6)
+    def update(self, tracked_objects, timestamp):
 
 
-            else:
-                self.monitored_objects.append(Monitor_object(obj[0],obj[1],obj[2], timestamp)) ## uusi objecti
+        for obj in tracked_objects:
+             if obj.estimate[obj.live_points].any():
+                 position = norfair.centroid(obj.estimate[obj.live_points])
+
+                 if position[0] > self.margins[0] or position[0] < self.margins[1]: # x cooridinate
+
+                    id = obj.id
+                    class_name = obj.last_detection.label
+
+                    if id in self.monitored_objects:
+
+                        index = self.monitored_objects.index(id)
+
+                        monitor_obj = self.monitored_objects[index]
+               
+                        monitor_obj.newPosition(position, timestamp = timestamp)
+
+                        if monitor_obj.class_name == self.speed_object:
+                            
+                            if self.avg_set == False:
+                                if monitor_obj.lenght_calculated == False:
+                                    if monitor_obj.direction != None:
+                                        points = obj.last_detection.points
+                                        measurements = abs(points[0][0] - points[1][0])
+                                        
+                                        if monitor_obj.direction == "West":
+                                            self.avg_w.append(measurements/self.avg_lenght)
+                                            
+                                        if monitor_obj.direction == "East":
+                                            self.avg_e.append(measurements/self.avg_lenght)
+
+                                        if len(self.avg_e) > 100 and len(self.avg_w) > 100:
+                                            self.avg_set = True
+
+                                        
+                                        monitor_obj.lenght_calculated = True
+
+                    else:
+                        self.monitored_objects.append(Monitor_object(id,class_name,position, timestamp)) ## new object
+
+
+                        
 
         for obj in self.monitored_objects:
             obj.life = obj.life - 1
 
             if obj.life == 0:
                 ##tappamis logikka
-                avg_speed = obj.kill()
-                print((avg_speed/self.px_to_m)*3.6)
+                
+                if obj.class_name == self.speed_object:
+                    avg_speed = obj.kill()
+                    if obj.direction == "West":
+                        
+                        avg = ((avg_speed/(sum(self.avg_w)/len(self.avg_w)))*3.6)
+                    if obj.direction == "East":
+                        
+                        avg = ((avg_speed/(sum(self.avg_e)/len(self.avg_e)))*3.6)
 
-                # -> 
-                # self.finished_objects.append
+                    self.finished_objects.append([obj.class_name,avg, obj.direction, timestamp ])
+
+                    print([obj.class_name,avg, obj.direction, timestamp ])
+                    
+
+                else:
+
+                    self.finished_objects.append([obj.class_name, obj.direction, timestamp ])
+
+                    print([obj.class_name, obj.direction, timestamp ])
+                
                 self.monitored_objects.remove(obj)
                         
 
 
         #print(self.monitored_objects)
+
+
                 
     
 
